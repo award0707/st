c9fcbbaa965e38e4e853bf62522f20fcd9bfc230
Andrew Ward <wardac@vcu.edu>
2024-07-15 18:39:53 -0400

    transprency fix for light modes

--- config.def.h
+++ config.def.h
@@ -99,7 +99,7 @@ char *termname = "st-256color";
  */
 unsigned int tabspaces = 8;
 
-float alpha = 0.8;
+float alpha = 0.9;
 
 typedef struct {
 	const char* const colors[258]; /* terminal colors */
